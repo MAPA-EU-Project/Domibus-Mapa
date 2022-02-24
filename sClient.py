135b528ec61119ed975f2be1fce2bd03e538b966
aestela-pangeanic <45590413+aestela-pangeanic@users.noreply.github.com>
2022-02-24 09:05:17 +0100

    Update sClient.py

--- sClient.py
+++ sClient.py
@@ -1,137 +1,27 @@
-from flask import Flask
-from flask import request
-import json
-import base64
-import os, time
-import threading
+import glob
+import os
 import time
 
+client_in_dir="/home/ubuntu/client/in/"
+client_out_dir="/home/ubuntu/client/out/"
+filestore="/home/ubuntu/filestore/"
+# root_dir needs a trailing slash (i.e. /root/dir/)
+error=False
+while not error:
+     #client out to filestore OUT
+     print("Checking client/out")
+     for filename in glob.iglob(client_out_dir + '**/*.txt', recursive=True):
+          command="mv " + filename + " " + filestore + "/OUT"
+          print(command)
+          os.system(command)
 
-indir="/home/ubuntu/filestore/IN"
-outdir="/home/ubuntu/filestore/OUT"
-
-path_to_watch = indir
-translated={}
-
-app = Flask(__name__)
-
-fIndex=0
-
-@app.route('/api/atranslatefile', methods = ['POST'])
-def atranslatefile():
-    global fIndex
-    fIndex = fIndex + 1
-    content = request.get_json()
-
-    fileType = content['fileType']
-    source = content['source']
-    token = content['token']
-    target = content['target']
-    encodedcontents=content['file']
-
-    print("request from",token, "(", source,target,fileType,")", "received", "Requestid:", fIndex)
-    decodedcontents=base64.b64decode(encodedcontents)
-    output_file = open(outdir+"/"+str(fIndex) + "_"+ token +  "_" + source + "_" + target +'.' + fileType , 'wb')
-    output_file.write(decodedcontents)
-    output_file.close()
-    result={
-        "success": "true",
-        "error": "null",
-        "data": {
-            "guid": fIndex
-        }
-    }
-    return json.dumps(result)
-
-
-
-@app.route('/api/aretrievefiletranslation', methods = ['POST'])
-def aretrievefiletranslation():
-    global translated
-    content = request.get_json()
-
-    guid = content['guid']
-    token = content['token']
-    print("Retrieve request for:", guid)
-    if guid in translated:
-        print("Retrieve request for:", guid, "found")
-        result={
-            "success": "true",
-            "error": "null",
-            "data": {
-                "guid": guid,
-                "fileType": "txt",
-                "file": translated[guid].decode('utf-8')
-                }
-            }
-        del translated[guid]
-    else:
-        print("Retrieve request for:", guid, "NOT found")
-        result ={
-            "success": "false",
-            "error": {
-                "statusCode": 400,
-                "code": 16,
-                "message": "Missing <guid>"
-                },
-            "data": "null"
-            }
-
-    return json.dumps(result)
-
-
-
-
-def watcher():
-    global translated
-    before={}
-    for root, subdirs, files in os.walk(path_to_watch):
-        for file in os.listdir(root):
-            filePath = os.path.join(root, file)
-            if os.path.isdir(filePath):
-                pass
-            else:
-                before[filePath]=None
-
-    #before = dict([(f, None) for f in os.listdir(path_to_watch)])
-    while 1:
-        time.sleep(1)
-        after={}
-        for root, subdirs, files in os.walk(path_to_watch):
-            for file in os.listdir(root):
-                filePath = os.path.join(root, file)
-                if os.path.isdir(filePath):
-                    pass
-                else:
-                    after[filePath]=None
-        #after = dict([(f, None) for f in os.listdir(path_to_watch)])
-
-
-        added = [f for f in after if not f in before]
-        removed = [f for f in before if not f in after]
-        if added:
-            print("Added: ", ", ".join(added))
-
-        if removed:
-            print("Removed: ", ", ".join(removed))
-        before = after
-
-        for f in added:
-            base = os.path.basename(f)
-            (name, ext) = os.path.splitext(base)
-            print ("added", base, name, ext)
-            if ext==".txt":
-                index = base.split("_")[0]
-                print("translated file index",index,"found:", base)
-                tt=""
-                with open(f,'r') as t:
-                    tt=t.readlines()
-                tt='\n'.join(tt)
-                print("file:", tt)
-                tu=tt.encode('utf-8')
-                encoded = base64.b64encode(tu)
-                print("Encoded:", encoded)
-                translated[index]=encoded
+     #filestore IN to client IN
+     print("checking filestore/IN")
+     for filename in glob.iglob(filestore+"IN/" + '**/*.txt', recursive=True):
+          command="mv " + filename + " " + client_in_dir + os.path.basename(filename)
+          print(command)
+          os.system(command)
+     time.sleep(5)
 
 
 
